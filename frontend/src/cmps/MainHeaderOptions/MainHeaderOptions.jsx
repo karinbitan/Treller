9a0dc886b5f9f71dcbaba991089b514176853796
Karin Bitan <bitankarin@gmail.com>
2021-02-13 20:45:00 +0200

    work in progress

--- frontend/src/cmps/MainHeaderOptions/MainHeaderOptions.jsx
+++ frontend/src/cmps/MainHeaderOptions/MainHeaderOptions.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { eventBus } from '../../services/eventBusService';
 import { socketService } from '../../services/socketService';
 import './MainHeaderOptions.scss';
 
